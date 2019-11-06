6f04ecc597180f4d0c794da1d5d1e4d61a37ce69
Victor Fong <vfong3@uic.edu>
2019-11-05 18:23:14 -0600

    init commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+
+
+function setup()
+{
+    createCanvas(400, 400);
+    createSprite(200, 200, 50, 50);
+}
+
+
+function draw()
+{
+    background(255, 255, 255);
+    drawSprites();
+}
